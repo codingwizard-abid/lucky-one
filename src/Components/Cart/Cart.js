e38d8316bb9013c5b81353e2cf85b2c92cdde052
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-03-26 00:55:42 +0600

    cart working

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -6,7 +6,7 @@ const Cart = ({cartItem}) => {
    return (
       <div>
          <div className="single-ball">
-            <img src={image} alt={name} />
+            <img src={image} alt={name}/>
             <h3>{name}</h3>
          </div>
       </div>
